99a82a5ddbeda398f7d517c157c55f1c998e418c
HimansuSekharSahu <gs.himanshu.ss@gmail.com>
2020-05-02 02:29:42 +0530

    Updates

---
+++ precache-manifest.1a31870f0391c9ad1204f397b9913fc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda2eb442568c7f9d3c39d509ced58fa",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "9fdda5566b0b64739e63",
+    "url": "/Monster-Rolodex/static/css/main.b2dba929.chunk.css"
+  },
+  {
+    "revision": "da2d19df4e7afbf119c5",
+    "url": "/Monster-Rolodex/static/js/2.80b00217.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.80b00217.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fdda5566b0b64739e63",
+    "url": "/Monster-Rolodex/static/js/main.938e931d.chunk.js"
+  },
+  {
+    "revision": "3bdc701e530878c5d7e7",
+    "url": "/Monster-Rolodex/static/js/runtime-main.70fbbf32.js"
+  }
+]);
